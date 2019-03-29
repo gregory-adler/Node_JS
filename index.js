79100cb0b5619e98dd6780037a4e0efcb4d0b022
Greg Adler <greg7239@gmail.com>
2019-03-29 14:13:51 -0700

    added rendering of stock

--- index.js
+++ index.js
@@ -8,24 +8,28 @@ var AlphaVantageAPI = require('alpha-vantage-cli').AlphaVantageAPI;
 var yourApiKey = 'K66X37W9RVFUC4RQ';
 var alphaVantageAPI = new AlphaVantageAPI(yourApiKey, 'compact', true);
 
+let stocks = []
+
 const getStocks = async () => {
   try {
   	return axios.get("https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol=MSFT&apikey=K66X37W9RVFUC4RQ&datatype=json&outputsize=compact").then(dailyData => {
-        	// console.log(dailyData.data['Time Series (Daily)'])
         	for (variable in dailyData.data['Time Series (Daily)']){
-        		console.log (dailyData.data['Time Series (Daily)'][variable]['4. close']);
-        		break;
+        		// console.log (dailyData.data['Time Series (Daily)'][variable]['4. close']);
+        		stocks.push(dailyData['data']['Meta Data']['2. Symbol']);
+        		stocks.push(dailyData.data['Time Series (Daily)'][variable]['4. close']);
+        		return stocks;
         	}
         	})
   	} catch(error){
   		console.error(error)
   	}}
 
-getStocks()
+
+getStocks().then(stocks => console.log(stocks))
 
 express()
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
+  .get('/', (req, res) => res.render('pages/index', {stocks:stocks}))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
