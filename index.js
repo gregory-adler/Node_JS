d7a33947584e432f50fd18763b017fe241ec1706
Greg Adler <greg7239@gmail.com>
2019-03-31 13:38:49 -0700

    added cryptocurrency prices

--- index.js
+++ index.js
@@ -9,7 +9,9 @@ let yourApiKey = 'K66X37W9RVFUC4RQ';
 let alphaVantageAPI = new AlphaVantageAPI(yourApiKey, 'compact', true);
 
 let stocks = []
+let cryptos = []
 let ticker = ['MSFT', 'NVDA', 'AAPL']
+let crypto_ticker = ['BTC', 'ETH',]
 
 // styles currency
 const formatter = new Intl.NumberFormat('en-US', {
@@ -20,29 +22,49 @@ const formatter = new Intl.NumberFormat('en-US', {
 
 const getStocks = async (ticker) => {
   try {
-  	console.log (ticker)
   	return axios.get(`https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol=${ticker}&apikey=K66X37W9RVFUC4RQ&datatype=json&outputsize=compact`).then(dailyData => {
 		for (variable in dailyData.data['Time Series (Daily)']){
 			// console.log (dailyData.data['Time Series (Daily)'][variable]['4. close']);
-			stocks.push(dailyData['data']['Meta Data']['2. Symbol']);
 			let value = formatter.format(dailyData.data['Time Series (Daily)'][variable]['4. close']);
- 			stocks.push(value);
+			stocks.push([ticker, value]);
 			break;
 		}
 		return stocks;
-	})}catch(error){
+	})}
+	catch(error){
+  		console.error(error)
+  	}}
+
+const getCryptos = async (crypto) => {
+	try {
+	return axios.get(`https://www.alphavantage.co/query?function=CURRENCY_EXCHANGE_RATE&from_currency=${crypto}&to_currency=USD&apikey=K66X37W9RVFUC4RQ`).then(dailyData => {
+			console.log(dailyData.data['Realtime Currency Exchange Rate']['5. Exchange Rate'])
+			let value = formatter.format(dailyData.data['Realtime Currency Exchange Rate']['5. Exchange Rate'])
+			cryptos.push([crypto, value])
+			return cryptos;
+	})}
+	catch(error){
   		console.error(error)
   	}}
 
 
+
+// stock calls 
 for (i=0; i< ticker.length; i++){
-  		// console.log (ticker[i]);
-  		getStocks(ticker[i]).then(stocks=> console.log(stocks))
+		// console.log (ticker[i]);
+		getStocks(ticker[i]).then(stocks=> console.log(stocks))
 	}
+
+// crypto calls
+for (i=0; i< crypto_ticker.length; i++){
+		getCryptos(crypto_ticker[i]).then(cryptos=> console.log(cryptos))
+	}
+
+
 
 express()
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index', {stocks:stocks}))
+  .get('/', (req, res) => res.render('pages/index', {stocks:stocks, cryptos:cryptos}))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
